08a7cd77d09547e582647db78424e1f0f3940feb
swapnilaanam <swapnilaanam211@gmail.com>
2023-03-20 05:16:52 +0600

    some more topics

---
+++ history.js
@@ -0,0 +1,4 @@
+console.log(history.length);
+// history.go(-2);
+// history.back();
+// history.forward();
---
+++ location.js
@@ -0,0 +1,12 @@
+console.log(location.href);
+console.log(location.toString());
+
+// location.reload();
+// location.assign('https://www.google.com/');
+// location.href = 'https://www.google.com/';
+// console.log(location.protocol);
+// console.log(location.hostname);
+// console.log(location.pathname);
+// console.log(location.port)
+// console.log(location.origin);
+// location.replace('https://www.google.com/');
